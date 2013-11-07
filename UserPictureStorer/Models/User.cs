46bb6ef1ace5dd9ea1ee4b5950cfd1a985eda4c1
Iñaki Elcoro <ielcoro@consultec.es>
2013-11-07 20:03:00 +0100

    Initial view creation

---
+++ UserPictureStorer/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace UserPictureStorer.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public string Age { get; set; }
+
+        public Image Picture { get; set; }
+    }
+}
