187cc4ee5f485ff0887a2cb4b4e529c853adcd1b
Iñaki Elcoro <ielcoro@consultec.es>
2013-11-07 20:07:52 +0100

    Create users

--- UserPictureStorer/Models/User.cs
+++ UserPictureStorer/Models/User.cs
@@ -8,6 +8,8 @@ namespace UserPictureStorer.Models
 {
     public class User
     {
+        public int Id { get; set; }
+
         public string Name { get; set; }
 
         public string Age { get; set; }
