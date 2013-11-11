9b527738deaa19942011c44655e5a37281ca2f75
Iñaki Elcoro <ielcoro@consultec.es>
2013-11-11 18:44:45 +0100

    Clean

--- UserPictureStorer/Properties/Settings.Designer.cs
+++ UserPictureStorer/Properties/Settings.Designer.cs
@@ -26,8 +26,6 @@ namespace UserPictureStorer.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("DefaultEndpointsProtocol=https;AccountName=demousers;AccountKey=OLFLdWavYCA7N8Y3G" +
-            "2WG+e64SAifRx+NeoMg3pLrJvxnBhAunt7HBxmKlL0ImTLTXmfTM6HVFufKPJjbSfAuow==")]
         public string DataConnection {
             get {
                 return ((string)(this["DataConnection"]));
